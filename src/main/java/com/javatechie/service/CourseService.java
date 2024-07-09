aa4a0fda45043eb56ca7d1b89ba2f96f7d007004
birudeogarande <birudeogarande@gmaill.com>
2024-07-09 15:18:37 +0530

    added default course list

--- src/main/java/com/javatechie/service/CourseService.java
+++ src/main/java/com/javatechie/service/CourseService.java
@@ -1,11 +1,13 @@
 package com.javatechie.service;
 
 import com.javatechie.dto.Course;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 public class CourseService {
@@ -13,6 +15,14 @@ public class CourseService {
     //RDS DB
     private final List<Course> courses = new ArrayList<>();
 
+    @PostConstruct
+    public void init(){
+        courses.add(new Course(new Random().nextInt(), "Java", 1000));
+        courses.add(new Course(new Random().nextInt(), "CSS", 100));
+        courses.add(new Course(new Random().nextInt(), "JavaScript", 500));
+        courses.add(new Course(new Random().nextInt(), "JSP", 200));
+        courses.add(new Course(new Random().nextInt(), "Sprig", 2000));
+    }
     // Create a new course
     public void addCourse(Course course) {
         courses.add(course);
